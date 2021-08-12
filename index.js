21e4b657549c631d8aa75c8d49cc4759a15e5779
jafri <syedjafri97@gmail.com>
2021-08-11 18:17:03 -0600

    1 file modified

--- index.js
+++ index.js
@@ -1,5 +1,28 @@
-// run `node index.js` in the terminal
+const { JsonRpc } = require('@proton/js');
+const fs = require('fs');
 
-console.log(`Hello Node.js v${process.versions.node}!`);
+const rpc = new JsonRpc(['https://protontestnet.greymass.com/']);
 
-const 
+const getCredentials = async lower_bound => {
+  const { rows, more, next_key } = await rpc.get_table_rows({
+    code: 'webauthn',
+    scope: 'webauthn',
+    table: 'credentials',
+    limit: -1,
+    lower_bound
+  });
+
+  if (more) {
+    return rows.concat(await getCredentials(next_key));
+  } else {
+    return rows;
+  }
+};
+
+const main = async () => {
+  const credentials = await getCredentials();
+
+  fs.writeFileSync('./creds.json', JSON.stringify(credentials, null, 4));
+};
+
+main();
